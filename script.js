573801a7ca0f823462aa4415f384d45ee8cce429
mariocstweb <mariocstmra@gmail.com>
2024-01-09 09:03:56 +0100

    added script

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("ok");
+// Creo la mia lista della spesa
+const shoppingList = [
+  "pane",
+  "uova",
+  "latte",
+  "salumi",
+  "carne",
+  "pesce",
+  "frutta",
+  "verdura",
+];
+
+// Recupero l elemento html dove voglio "stampare" la mia lista
+// const content = document.getElementById("list");
+// let listHtml = "<ul>";
+
+// for (let i = 0; i < shoppingList.length; i++) {
+//   listHtml += `<li>${shoppingList[i]}</li>`;
+// }
+
+// listHtml += "</ul>";
+// content.innerHTML = listHtml;
+
+// Replico il tutto utilizzando il "while"
+
+const content = document.getElementById("list");
+let listHtml = "<ul>";
+
+let i = 0;
+while (i < shoppingList.length) {
+  listHtml += `<li>${shoppingList[i]}</li>`;
+  i++;
+}
+
+listHtml += "</ul>";
+content.innerHTML = listHtml;
